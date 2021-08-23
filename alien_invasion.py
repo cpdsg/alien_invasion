343c98bd65c1eeaa5edc4dd078bb891b882da823
cpdsg <754520684@qq.com>
2021-08-24 00:44:41 +0800

    1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,13 @@
 import sys
 import pygame
 class AlienInvasion:
+
+    def __init__(self):
+ #   """管理游戏资源和行为的类"""
+      pygame.init()
+      self.screen = pygame.display.set_mode((1200.800))
+      pygame.display.set_caption("Alien Invasion")
+
+
     
+  
