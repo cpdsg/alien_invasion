8cade2a20108b7692c027f2ca6d0fea01f918f99
cpdsg <754520684@qq.com>
2021-08-24 20:52:18 +0800

    添加了飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,35 @@
 import sys
 import pygame
+from settings import Settings
 class AlienInvasion:
 
     def __init__(self):
- #   """管理游戏资源和行为的类"""
+      """管理游戏资源和行为的类"""
       pygame.init()
-      self.screen = pygame.display.set_mode((1200.800))
-      pygame.display.set_caption("Alien Invasion")
+      self.settings = Settings()
 
+      self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+      pygame.display.set_caption("Alien Invasion")
+      #设置背景色
+      self.bg_color = (230 ,230 ,230)
+    
+    def run_game(self):
+        """开始游戏主循环"""
+        while True:
+            #监视键盘和鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #每次循环重新绘制屏幕
+            self.screen.fill(self.settings.bg_color)        
+            #让最近绘制的图片可见
+            pygame.display.flip()
+    
+if __name__ == "__main__":
+    #创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
+        
 
     
   
